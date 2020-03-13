015ef097813e05b56295fa941671411cd63f2a35
siseko Neti <sisekoreggie@users.noreply.github.com>
2020-03-13 03:08:12 +0200

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ function sendEmail(emailBody) {
 	Email.send({
 	Host: "smtp.sendgrid.net",
 	Username : "apikey",
-	Password : "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",
+	Password : ${{ secrets.SendGrid_Api_Key }},
 	To : 'mpilo@capealluminiumkings.com',
 	From : "njataabo@gmail.com",
 	Subject : "Contact us",
