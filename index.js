ba998ed056b2adfa2c39588b02a4bc23e23b10d5
sisekoreggie <3443307@myuwc.ac.za>
2020-03-13 03:15:24 +0200

    Updated sendgrid keys

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ function sendEmail(emailBody) {
 	Email.send({
 	Host: "smtp.sendgrid.net",
 	Username : "apikey",
-	Password : ${{ secrets.SendGrid_Api_Key }},
+	Password : secrets.SendGrid_Api_Key,
 	To : 'mpilo@capealluminiumkings.com',
 	From : "njataabo@gmail.com",
 	Subject : "Contact us",
